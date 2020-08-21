b6b0933689e611f51d070f99d7cf742f19425784
bed72 <gabriel.jr@aluno.ifsc.edu.br>
2020-08-21 13:10:18 -0300

    :sparkles:

--- app/src/main/java/com/bed/gosale/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bed/gosale/ui/splash/SplashActivity.kt
@@ -1,10 +1,29 @@
 package com.bed.gosale.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.bed.gosale.MainActivity
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private val handler = Handler()
+
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 5000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
     }
 }
